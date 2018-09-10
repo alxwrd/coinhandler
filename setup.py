8713afd620d2501890ca48fce83be267e940e610
Alex Ward <alxwrd@googlemail.com>
2018-09-10 21:10:26 +0100

    add setup.py for installing as a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='coinhandler',
+    packages=['coinhandler'],
+    version='1.0',
+    description='',
+    author='Alex Ward',
+    author_email='alxwrd@googlemail.com',
+    url='https://github.com/alxwrd/coins',
+    long_description=open('README.md', encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    license='MIT',
+    python_requires='>=3.6',
+    project_urls={
+        'Source': 'https://github.com/alxwrd/coinhandler',
+        'Bug Reports':  'https://github.com/alxwrd/coins/coinhandler',
+    }
+)
