65b27574b6ee238980e0f3d6099d7f0f1f072f57
Alex Ward <alxwrd@googlemail.com>
2018-09-12 23:08:03 +0100

    add tests for new subclass searching

--- tests/test_coins.py
+++ tests/test_coins.py
@@ -1,6 +1,6 @@
 import pytest
 
-from coinhandler import Pound, Pence
+from coinhandler import Pound, Pence, Coin
 
 
 def test_pounds_are_equal():
@@ -31,3 +31,29 @@ def test_coins_are_comparable_to_ints():
 
     assert pound == 100
     assert pence == 20
+
+
+def test_coin_gets_correct_subclass():
+    assert Coin(100) == Pound(1)
+
+
+def test_new_subtypes_of_coin():
+    class Fifty(Coin):
+        multiplier = 50
+
+    assert Coin(50) == Fifty(1)
+
+
+def test_coin_can_get_correct_subclass_from_new_coins():
+    class Fifty(Coin):
+        multiplier = 50
+
+    class Twenty(Coin):
+        multiplier = 20
+
+    class Ten(Coin):
+        multiplier = 10
+
+    assert Coin(50) == Fifty(1)
+    assert Coin(20) == Twenty(1)
+    assert Coin(10) == Ten(1)
