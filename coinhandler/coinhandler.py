6dbee87ce675df37abed67c83f36aee8e10b281b
Alex Ward <alxwrd@googlemail.com>
2018-09-13 18:24:53 +0100

    add insert method to the handler

--- coinhandler/coinhandler.py
+++ coinhandler/coinhandler.py
@@ -1,10 +1,14 @@
-from .coins import CoinCollection
+from .coins import CoinCollection, Transaction
 
 
 class CoinHandler:
 
     def __init__(self, starting_float=()):
         self.available_coins = CoinCollection(*starting_float)
+        self.current_transaction = Transaction()
 
     def total(self):
         return self.available_coins.total() / 100
+
+    def insert(self, value):
+        self.current_transaction.append(value)
