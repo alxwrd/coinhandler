7ce349724b8f5615e418f520fb0b5ec4227d7bd1
Alex Ward <alxwrd@googlemail.com>
2018-09-15 11:20:10 +0100

    add some tests around a coincollection
    tests for appending, extending, and equality

--- tests/test_coins.py
+++ tests/test_coins.py
@@ -44,3 +44,23 @@ def test_coin_collections_are_equal():
 
 def test_creating_collection_from_value():
     assert CoinCollection.from_value(1.25) == CoinCollection(100, 20, 5)
+
+
+def test_appending_to_collection():
+    collection = CoinCollection(1, 2)
+
+    collection.append(3)
+
+    assert collection == CoinCollection(1, 2, 3)
+
+
+def test_extending_collection():
+    collection = CoinCollection(1, 2)
+
+    collection.extend([3, 4])
+
+    assert collection == CoinCollection(1, 2, 3, 4)
+
+
+def test_collection_equal_to_list():
+    assert CoinCollection(1, 2, 3) == [1, 2, 3]
